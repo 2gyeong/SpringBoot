c43ac3956e38756f9f23d854b774c4c1f21442f7
Yigyeong <knk222@naver.com>
2023-03-21 18:29:16 +0900

    23.03.21.

---
+++ cartking/src/main/java/com/cartking/CartkingApplication.java
@@ -0,0 +1,13 @@
+package com.cartking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartkingApplication.class, args);
+	}
+
+}
---
+++ cartking/src/test/java/com/cartking/CartkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cartking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
